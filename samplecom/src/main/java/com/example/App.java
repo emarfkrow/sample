a13f2aff33bf0cff6b7b5fcc73bb1eb016fc9954
ToshiyukiFukuo <t.fukuo@gmail.com>
2026-01-30 21:14:20 +0900

    ファイル整理

---
+++ samplecom/src/main/java/com/example/App.java
@@ -0,0 +1,22 @@
+package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+public final class App {
+    /**
+     * エントリポイント
+     *
+     * @param args コマンドライン引数
+     */
+    public static void main(final String[] args) {
+        System.out.println("Hello World!");
+    }
+
+    /**
+     * プライベートコンストラクタ
+     */
+    private App() {
+    }
+}
