f4c46fb773edfa4e83027895bfb7fdfdf901c244
ToshiyukiFukuo <t.fukuo.0002@gmail.com>
2026-02-14 13:56:46 +0900

    date_format

--- samplecom/src/main/resources/sql/T02EldestSearch.sql
+++ samplecom/src/main/resources/sql/T02EldestSearch.sql
@@ -1,14 +1,22 @@
 SELECT
       a.`BRO_ID`
     , a.`ELDEST_INFO`
-    , a.`INSERT_TS` AS `INSERT_TS`
+    , LEFT(DATE_FORMAT (a.`INSERT_TS`, '%Y-%m-%dT%H:%i:%s.%f'), 23) AS `INSERT_TS`
     , a.`INSERT_USER_ID`
     , (SELECT r0.`USER_SEI` FROM MHR_USER r0 WHERE r0.`USER_ID` = a.`INSERT_USER_ID`) AS `INSERT_USER_SEI`
-    , a.`UPDATE_TS` AS `UPDATE_TS`
+    , LEFT(DATE_FORMAT (a.`UPDATE_TS`, '%Y-%m-%dT%H:%i:%s.%f'), 23) AS `UPDATE_TS`
     , a.`UPDATE_USER_ID`
     , (SELECT r1.`USER_SEI` FROM MHR_USER r1 WHERE r1.`USER_ID` = a.`UPDATE_USER_ID`) AS `UPDATE_USER_SEI`
+    , c1.`YOUNGER_INFO`
+    , c2.`YOUNGEST_INFO`
 FROM
     T02_ELDEST a 
+    INNER JOIN T02_YOUNGER c1 
+        ON 1 = 1 
+        AND c1.BRO_ID = a.BRO_ID 
+    INNER JOIN T02_YOUNGEST c2 
+        ON 1 = 1 
+        AND c2.BRO_ID = a.BRO_ID 
 WHERE
     1 = 1 
     AND a.`BRO_ID` = :bro_id 
