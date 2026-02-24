f8d76be0601504d1d33e9543dd36e99a05941e0a
ToshiyukiFukuo <t.fukuo.0002@gmail.com>
2026-02-24 20:16:21 +0900

    認可制御

--- sampleweb/src/main/java/com/example/form/LoginForm.java
+++ sampleweb/src/main/java/com/example/form/LoginForm.java
@@ -1,7 +1,8 @@
 package com.example.form;
 
-import java.util.LinkedHashSet;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
 
 import jp.co.golorp.emarf.form.base.LoginFormBase;
 
@@ -11,48 +12,43 @@ import jp.co.golorp.emarf.form.base.LoginFormBase;
 public class LoginForm extends LoginFormBase {
 
     @Override
-    public final String getTableAuthZ(final String requestURI) {
+    public final Map<String, Integer> getTableAuthZ(final String tableName) {
 
-        Set<String> errorIds = new LinkedHashSet<String>();
+        Map<String, Integer> tableAuthZ = new HashMap<String, Integer>();
 
-        String[] requestURIs = requestURI.split("/");
-        String pagePath = requestURIs[requestURIs.length - 1];
-        String pageName = pagePath.replaceFirst("\\..+$", "");
+        for (Entry<String, Integer> e : this.getAuthzInfo().entrySet()) {
+            String tableRegex = e.getKey();
+            Integer kengenBF = e.getValue();
 
-        for (String k : this.getAuthzInfo().keySet()) {
-            if (pageName.matches(k)) {
-                Object v = this.getAuthzInfo().get(k);
-
-                int authz = Integer.parseInt(v.toString());
-
-                if ((authz & 0b00000001) == 0) {
-                    errorIds.add("error.authz.view"); // 第１ビット：照会権限
+            if (tableName.matches(tableRegex)) {
+                if ((kengenBF & 0b00000001) != 0) {
+                    tableAuthZ.put("view", 1); // 第１ビット：照会権限：1
                 }
-                if ((authz & 0b00000010) == 0) {
-                    errorIds.add("error.authz.seek"); // 第２ビット：検索権限
+                if ((kengenBF & 0b00000010) != 0) {
+                    tableAuthZ.put("seek", 1); // 第２ビット：検索権限：2
                 }
-                if ((authz & 0b00000100) == 0) {
-                    errorIds.add("error.authz.output"); // 第３ビット：出力権限
+                if ((kengenBF & 0b00000100) != 0) {
+                    tableAuthZ.put("output", 1); // 第３ビット：出力権限：4
                 }
-                if ((authz & 0b00001000) == 0) {
-                    errorIds.add("error.authz.anew"); // 第４ビット：追加権限
+                if ((kengenBF & 0b00001000) != 0) {
+                    tableAuthZ.put("anew", 1); // 第４ビット：追加権限：8
                 }
-                if ((authz & 0b00010000) == 0) {
-                    errorIds.add("error.authz.edit"); // 第５ビット：更新権限
+                if ((kengenBF & 0b00010000) != 0) {
+                    tableAuthZ.put("edit", 1); // 第５ビット：更新権限：16
                 }
-                if ((authz & 0b00100000) == 0) {
-                    errorIds.add("error.authz.delete"); // 第６ビット：削除権限
+                if ((kengenBF & 0b00100000) != 0) {
+                    tableAuthZ.put("delete", 1); // 第６ビット：削除権限：32
                 }
-                if ((authz & 0b01000000) == 0) {
-                    errorIds.add("error.authz.permit"); // 第７ビット：承認権限
+                if ((kengenBF & 0b01000000) != 0) {
+                    tableAuthZ.put("permit", 1); // 第７ビット：承認権限：64
                 }
-                if ((authz & 0b10000000) == 0) {
-                    errorIds.add("error.authz.forbid"); // 第８ビット：否認権限
+                if ((kengenBF & 0b10000000) != 0) {
+                    tableAuthZ.put("forbid", 1); // 第８ビット：否認権限：128
                 }
             }
         }
 
-        return null;
+        return tableAuthZ;
     }
 
 }
