d424d7df3c6e32b709573416ef00a0660d51a25f
ToshiyukiFukuo <t.fukuo.0002@gmail.com>
2026-03-12 20:53:13 +0900

    viewのcrud廃止

--- samplecom/src/main/java/com/example/entity/V13Furiwake.java
+++ samplecom/src/main/java/com/example/entity/V13Furiwake.java
@@ -1,6 +1,5 @@
 package com.example.entity;
 
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -53,15 +52,18 @@ public class V13Furiwake implements IEntity {
     }
 
     /** SRC_ID$DEST_ID */
+    @jp.co.golorp.emarf.validation.PrimaryKeys
     private Integer srcIdDestId;
 
     /** @return SRC_ID$DEST_ID */
     @com.fasterxml.jackson.annotation.JsonProperty(value = "SRC_ID$DEST_ID", index = 3)
+    @jp.co.golorp.emarf.validation.PrimaryKeys
     public Integer getSrcIdDestId() {
         return this.srcIdDestId;
     }
 
     /** @param o SRC_ID$DEST_ID */
+    @jp.co.golorp.emarf.validation.PrimaryKeys
     public void setSrcIdDestId(final Object o) {
         if (!jp.co.golorp.emarf.lang.StringUtil.isNullOrWhiteSpace(o)) {
             this.srcIdDestId = Integer.valueOf(o.toString());
@@ -108,92 +110,5 @@ public class V13Furiwake implements IEntity {
         sql += String.join(" AND \n", whereList);
         Map<String, Object> map = new HashMap<String, Object>();
         return Queries.get(sql, map, V13Furiwake.class);
-    }
-
-    /**
-     * VIEW追加
-     * @param now システム日時
-     * @param execId 登録者
-     * @return 追加件数
-     */
-    public int insert(final LocalDateTime now, final String execId) {
-
-        // VIEWの登録
-        String sql = "INSERT INTO V13_FURIWAKE(\r\n      " + names() + "\r\n) VALUES (\r\n      " + values() + "\r\n)";
-        return Queries.regist(sql, toMap(now, execId));
-    }
-
-    /** @return insert用のname句 */
-    private String names() {
-        List<String> nameList = new ArrayList<String>();
-        nameList.add("`TABLE_NAME` -- :table_name");
-        nameList.add("`SRC_ID$DEST_ID` -- :src_id_dest_id");
-        nameList.add("`INFO` -- :info");
-        return String.join("\r\n    , ", nameList);
-    }
-
-    /** @return insert用のvalue句 */
-    private String values() {
-        List<String> valueList = new ArrayList<String>();
-        valueList.add(":table_name");
-        valueList.add(":src_id_dest_id");
-        valueList.add(":info");
-        return String.join("\r\n    , ", valueList);
-    }
-
-    /**
-     * VIEW更新
-     * @param now システム日時
-     * @param execId 更新者
-     * @return 更新件数
-     */
-    public int update(final LocalDateTime now, final String execId) {
-
-        // VIEWの登録
-        String sql = "UPDATE V13_FURIWAKE\r\nSET\r\n      " + getSet() + "\r\nWHERE\r\n    " + getWhere();
-        return Queries.regist(sql, toMap(now, execId));
-    }
-
-    /** @return update用のset句 */
-    private String getSet() {
-        List<String> setList = new ArrayList<String>();
-        setList.add("`TABLE_NAME` = :table_name");
-        setList.add("`SRC_ID$DEST_ID` = :src_id_dest_id");
-        setList.add("`INFO` = :info");
-        return String.join("\r\n    , ", setList);
-    }
-
-    /**
-     * VIEW削除
-     * @return 削除件数
-     */
-    public int delete() {
-
-        // VIEWの削除
-        String sql = "DELETE FROM V13_FURIWAKE WHERE " + getWhere();
-        return Queries.regist(sql, toMap(null, null));
-    }
-
-    /** @return where句 */
-    private String getWhere() {
-        List<String> whereList = new ArrayList<String>();
-        return String.join(" AND ", whereList);
-    }
-
-    /**
-     * @param now システム日時
-     * @param execId 実行ID
-     * @return マップ化したエンティティ
-     */
-    private Map<String, Object> toMap(final LocalDateTime now, final String execId) {
-        Map<String, Object> map = new HashMap<String, Object>();
-        map.put("table_name", this.tableName);
-        map.put("src_id$dest_id", this.srcIdDestId);
-        map.put("info", this.info);
-        map.put("insert_ts", now);
-        map.put("insert_user_id", execId);
-        map.put("update_ts", now);
-        map.put("update_user_id", execId);
-        return map;
     }
 }
--- samplecom/src/main/java/com/example/entity/V13Henkan.java
+++ samplecom/src/main/java/com/example/entity/V13Henkan.java
@@ -1,6 +1,5 @@
 package com.example.entity;
 
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -53,15 +52,18 @@ public class V13Henkan implements IEntity {
     }
 
     /** 変換元ID */
+    @jp.co.golorp.emarf.validation.PrimaryKeys
     private Integer srcId;
 
     /** @return 変換元ID */
     @com.fasterxml.jackson.annotation.JsonProperty(value = "src_id", index = 3)
+    @jp.co.golorp.emarf.validation.PrimaryKeys
     public Integer getSrcId() {
         return this.srcId;
     }
 
     /** @param o 変換元ID */
+    @jp.co.golorp.emarf.validation.PrimaryKeys
     public void setSrcId(final Object o) {
         if (!jp.co.golorp.emarf.lang.StringUtil.isNullOrWhiteSpace(o)) {
             this.srcId = Integer.valueOf(o.toString());
@@ -108,92 +110,5 @@ public class V13Henkan implements IEntity {
         sql += String.join(" AND \n", whereList);
         Map<String, Object> map = new HashMap<String, Object>();
         return Queries.get(sql, map, V13Henkan.class);
-    }
-
-    /**
-     * VIEW追加
-     * @param now システム日時
-     * @param execId 登録者
-     * @return 追加件数
-     */
-    public int insert(final LocalDateTime now, final String execId) {
-
-        // VIEWの登録
-        String sql = "INSERT INTO V13_HENKAN(\r\n      " + names() + "\r\n) VALUES (\r\n      " + values() + "\r\n)";
-        return Queries.regist(sql, toMap(now, execId));
-    }
-
-    /** @return insert用のname句 */
-    private String names() {
-        List<String> nameList = new ArrayList<String>();
-        nameList.add("`TABLE_NAME` -- :table_name");
-        nameList.add("`SRC_ID` -- :src_id");
-        nameList.add("`DEST_INFO` -- :dest_info");
-        return String.join("\r\n    , ", nameList);
-    }
-
-    /** @return insert用のvalue句 */
-    private String values() {
-        List<String> valueList = new ArrayList<String>();
-        valueList.add(":table_name");
-        valueList.add(":src_id");
-        valueList.add(":dest_info");
-        return String.join("\r\n    , ", valueList);
-    }
-
-    /**
-     * VIEW更新
-     * @param now システム日時
-     * @param execId 更新者
-     * @return 更新件数
-     */
-    public int update(final LocalDateTime now, final String execId) {
-
-        // VIEWの登録
-        String sql = "UPDATE V13_HENKAN\r\nSET\r\n      " + getSet() + "\r\nWHERE\r\n    " + getWhere();
-        return Queries.regist(sql, toMap(now, execId));
-    }
-
-    /** @return update用のset句 */
-    private String getSet() {
-        List<String> setList = new ArrayList<String>();
-        setList.add("`TABLE_NAME` = :table_name");
-        setList.add("`SRC_ID` = :src_id");
-        setList.add("`DEST_INFO` = :dest_info");
-        return String.join("\r\n    , ", setList);
-    }
-
-    /**
-     * VIEW削除
-     * @return 削除件数
-     */
-    public int delete() {
-
-        // VIEWの削除
-        String sql = "DELETE FROM V13_HENKAN WHERE " + getWhere();
-        return Queries.regist(sql, toMap(null, null));
-    }
-
-    /** @return where句 */
-    private String getWhere() {
-        List<String> whereList = new ArrayList<String>();
-        return String.join(" AND ", whereList);
-    }
-
-    /**
-     * @param now システム日時
-     * @param execId 実行ID
-     * @return マップ化したエンティティ
-     */
-    private Map<String, Object> toMap(final LocalDateTime now, final String execId) {
-        Map<String, Object> map = new HashMap<String, Object>();
-        map.put("table_name", this.tableName);
-        map.put("src_id", this.srcId);
-        map.put("dest_info", this.destInfo);
-        map.put("insert_ts", now);
-        map.put("insert_user_id", execId);
-        map.put("update_ts", now);
-        map.put("update_user_id", execId);
-        return map;
     }
 }
