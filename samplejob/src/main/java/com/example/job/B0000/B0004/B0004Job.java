63a191e0fce8689d5d2fda4cebe0013a4ec149e9
ToshiyukiFukuo <t.fukuo.0002@gmail.com>
2026-04-26 19:52:00 +0900

    CSV出力

--- samplejob/src/main/java/com/example/job/B0000/B0004/B0004Job.java
+++ samplejob/src/main/java/com/example/job/B0000/B0004/B0004Job.java
@@ -1,31 +1,32 @@
 package com.example.job.B0000.B0004;
 
+import java.io.File;
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.dataformat.csv.CsvMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvSchema;
 
-import com.example.entity.T01Oya;
-
-import jp.co.golorp.emarf.exception.AppError;
+import jp.co.golorp.emarf.exception.SysError;
 import jp.co.golorp.emarf.job.BaseJob;
 import jp.co.golorp.emarf.sql.Queries;
 import jp.co.golorp.emarf.util.MapList;
 
 /**
- * CSVファイルの入出力
+ * CSVファイルの出力
  *
  * @author toshiyuki
  *
  */
 public class B0004Job extends BaseJob {
 
-    /**
-     * logger
-     */
-    private static final Logger LOG = LoggerFactory.getLogger(B0004Job.class);
+    //    /**
+    //     * logger
+    //     */
+    //    private static final Logger LOG = LoggerFactory.getLogger(B0004Job.class);
 
     /**
      * エントリポイント
@@ -45,20 +46,25 @@ public class B0004Job extends BaseJob {
      */
     public void running(final LocalDateTime now, final String jobId, final String[] args) {
 
-        // 追加
-        T01Oya e = new T01Oya();
-        e.setOyaInfo("親情報");
-        if (e.insert(now, jobId) != 1) {
-            throw new AppError("error.cant.insert");
-        }
-
+        // 外部SQLファイルでデータ取得
         String sql = this.loadSqlFile("B0004");
         Map<String, Object> params = new HashMap<String, Object>();
         MapList list = Queries.select(sql, params, null, null);
 
-        LOG.debug("T01_OYA:");
-        for (Map<String, Object> map : list) {
-            LOG.debug(map.toString());
+        String csvPath = "output_fixed.csv";
+
+        CsvSchema schema = CsvSchema.builder()
+                .addColumn("OYA_ID")
+                .addColumn("OYA_INFO")
+                .build()
+                .withHeader();
+
+        CsvMapper mapper = new CsvMapper();
+        mapper.configure(JsonGenerator.Feature.IGNORE_UNKNOWN, true);
+        try {
+            mapper.writer(schema).writeValue(new File(csvPath), list);
+        } catch (IOException e1) {
+            throw new SysError(e1);
         }
     }
 
