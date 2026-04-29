5ad54b67bde22525d559fc6ea4dae8b04cf15d71
ToshiyukiFukuo <t.fukuo.0002@gmail.com>
2026-04-29 09:08:49 +0900

    ファイル出力

--- samplejob/src/main/java/com/example/job/B0000/B0004/B0004Job.java
+++ samplejob/src/main/java/com/example/job/B0000/B0004/B0004Job.java
@@ -1,19 +1,11 @@
 package com.example.job.B0000.B0004;
 
-import java.io.File;
-import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
-import com.fasterxml.jackson.core.JsonGenerator;
-import com.fasterxml.jackson.dataformat.csv.CsvMapper;
-import com.fasterxml.jackson.dataformat.csv.CsvSchema;
-
-import jp.co.golorp.emarf.exception.SysError;
 import jp.co.golorp.emarf.job.BaseJob;
-import jp.co.golorp.emarf.sql.Queries;
-import jp.co.golorp.emarf.util.MapList;
+import jp.co.golorp.emarf.report.TextUtil;
 
 /**
  * CSVファイルの出力
@@ -46,26 +38,11 @@ public class B0004Job extends BaseJob {
      */
     public void running(final LocalDateTime now, final String jobId, final String[] args) {
 
-        // 外部SQLファイルでデータ取得
         String sql = this.loadSqlFile("B0004");
         Map<String, Object> params = new HashMap<String, Object>();
-        MapList list = Queries.select(sql, params, null, null);
 
-        String csvPath = "output_fixed.csv";
-
-        CsvSchema schema = CsvSchema.builder()
-                .addColumn("OYA_ID")
-                .addColumn("OYA_INFO")
-                .build()
-                .withHeader();
-
-        CsvMapper mapper = new CsvMapper();
-        mapper.configure(JsonGenerator.Feature.IGNORE_UNKNOWN, true);
-        try {
-            mapper.writer(schema).writeValue(new File(csvPath), list);
-        } catch (IOException e1) {
-            throw new SysError(e1);
-        }
+        TextUtil.csv(sql, params, "out/output_fixed.csv");
+        TextUtil.tsv(sql, params, "out/output_fixed.tsv");
     }
 
 }
