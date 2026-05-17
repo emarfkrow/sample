6a844d9808bc45058a0981329ac7fae4c7256c3a
ToshiyukiFukuo <t.fukuo.0002@gmail.com>
2026-05-17 21:55:21 +0900

    SQL修正

--- sampleweb/src/main/webapp/WEB-INF/sql/D0000/D0001Search.sql
+++ sampleweb/src/main/webapp/WEB-INF/sql/D0000/D0001Search.sql
@@ -1,6 +1,6 @@
 SELECT
-'D0002'as table_name,
-      o.oya_id
+      'D0002' AS table_name
+    , o.oya_id
     , o.oya_info
     , k.ko_bn
     , k.ko_info 
@@ -10,10 +10,10 @@ FROM
         ON k.oya_id = o.oya_id 
 WHERE
     1 = 1 
-    AND o.oya_id = :oya_id 
-    AND UPPER (RTRIM (RTRIM (o."OYA_INFO"), '�@')) LIKE UPPER ('%' || :oya_info || '%') 
-    AND o.ko_bn = :ko_bn 
-    AND UPPER (RTRIM (RTRIM (o."KO_INFO"), '�@')) LIKE UPPER ('%' || :ko_info || '%') 
+    AND a.`OYA_ID` = :oya_id 
+    AND UPPER (TRIM (TRAILING ' ' FROM a.`OYA_INFO`)) LIKE UPPER (CONCAT ('%', :oya_info, '%')) 
+    AND a.`KO_BN` = :ko_bn 
+    AND UPPER (TRIM (TRAILING ' ' FROM a.`KO_INFO`)) LIKE UPPER (CONCAT ('%', :ko_info, '%')) 
 ORDER BY
     o.oya_id
     , k.ko_bn
