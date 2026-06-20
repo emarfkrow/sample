54a8ac3e691531270487f4faa362e625ce6992a6
ToshiyukiFukuo <t.fukuo.0002@gmail.com>
2026-06-20 13:33:59 +0900

    ガント対応中

--- samplecom/src/main/resources/META-INF/resources/model/T00KouteiGanttTasks.js
+++ samplecom/src/main/resources/META-INF/resources/model/T00KouteiGanttTasks.js
@@ -16,7 +16,17 @@ let T00KouteiGanttTasks = {
             task.name = row.KOUTEI_MEI;
             task.start = row.KAISHI_BI;
             task.end = row.SHURYO_BI;
-            task.dependencies = row.OYA_KOUTEI_ID ? row.OYA_KOUTEI_ID + '' : '';
+            task.dependencies = row.DEPENDENCIES ? row.DEPENDENCIES + '' : '';
+
+            task.kouteiId = row.KOUTEI_ID;
+            task.kouteiMei = row.KOUTEI_MEI;
+            task.kaishiBi = row.KAISHI_BI;
+            task.shuryoBi = row.SHURYO_BI;
+            task.oyaKouteiId = row.OYA_KOUTEI_ID;
+            task.insertTs = row.INSERT_TS;
+            task.insertUserId = row.INSERT_USER_ID;
+            task.updateTs = row.UPDATE_TS;
+            task.updateUserId = row.UPDATE_USER_ID;
 
             tasks.push(task);
         }
