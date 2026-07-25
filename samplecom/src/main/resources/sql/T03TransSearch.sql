a54b5f6f0b168e9292601600afd66ee34d4289b8
ToshiyukiFukuo <t.fukuo.0002@gmail.com>
2026-07-25 10:56:52 +0900

    Oracle版でグラフ・ガント見直し

--- samplecom/src/main/resources/sql/T03TransSearch.sql
+++ samplecom/src/main/resources/sql/T03TransSearch.sql
@@ -1,27 +1,27 @@
 SELECT
-      a.`TRANS_ID` AS `TRANS_ID`
-    , a.`TRANS_INFO` AS `TRANS_INFO`
-    , a.`STATUS_KB` AS `STATUS_KB`
-    , LEFT(DATE_FORMAT (a.`INSERT_TS`, '%Y-%m-%dT%H:%i:%s.%f'), 23) AS `INSERT_TS`
-    , TRIM(TRAILING ' ' FROM a.`INSERT_USER_ID`) AS `INSERT_USER_ID`
-    , (SELECT r0.`USER_SEI` FROM MHR_USER r0 WHERE r0.`USER_ID` = a.`INSERT_USER_ID`) AS `INSERT_USER_SEI`
-    , LEFT(DATE_FORMAT (a.`UPDATE_TS`, '%Y-%m-%dT%H:%i:%s.%f'), 23) AS `UPDATE_TS`
-    , TRIM(TRAILING ' ' FROM a.`UPDATE_USER_ID`) AS `UPDATE_USER_ID`
-    , (SELECT r1.`USER_SEI` FROM MHR_USER r1 WHERE r1.`USER_ID` = a.`UPDATE_USER_ID`) AS `UPDATE_USER_SEI`
+      a."TRANS_ID" AS "TRANS_ID"
+    , a."TRANS_INFO" AS "TRANS_INFO"
+    , a."STATUS_KB" AS "STATUS_KB"
+    , TO_CHAR (a."INSERT_TS", 'YYYY-MM-DD HH24:MI:SS.FF3') AS "INSERT_TS"
+    , RTRIM (RTRIM (a."INSERT_USER_ID"), '　') AS "INSERT_USER_ID"
+    , (SELECT r0."USER_SEI" FROM MHR_USER r0 WHERE TO_CHAR (r0."USER_ID") = a."INSERT_USER_ID") AS "INSERT_USER_SEI"
+    , TO_CHAR (a."UPDATE_TS", 'YYYY-MM-DD HH24:MI:SS.FF3') AS "UPDATE_TS"
+    , RTRIM (RTRIM (a."UPDATE_USER_ID"), '　') AS "UPDATE_USER_ID"
+    , (SELECT r1."USER_SEI" FROM MHR_USER r1 WHERE TO_CHAR (r1."USER_ID") = a."UPDATE_USER_ID") AS "UPDATE_USER_SEI"
 FROM
     T03_TRANS a 
 WHERE
     1 = 1 
-    AND a.`TRANS_ID` = :trans_id 
-    AND UPPER (TRIM(TRAILING ' ' FROM a.`TRANS_INFO`)) LIKE UPPER (CONCAT ('%', :trans_info, '%')) 
-    AND TRIM(TRAILING ' ' FROM a.`STATUS_KB`) IN (:status_kb) 
-    AND a.`INSERT_TS` = :insert_ts 
-    AND a.`INSERT_TS` >= :insert_ts_1 
-    AND a.`INSERT_TS` <= :insert_ts_2 
-    AND UPPER (TRIM(TRAILING ' ' FROM a.`INSERT_USER_ID`)) LIKE UPPER (CONCAT ('%', :insert_user_id, '%')) 
-    AND a.`UPDATE_TS` = :update_ts 
-    AND a.`UPDATE_TS` >= :update_ts_1 
-    AND a.`UPDATE_TS` <= :update_ts_2 
-    AND UPPER (TRIM(TRAILING ' ' FROM a.`UPDATE_USER_ID`)) LIKE UPPER (CONCAT ('%', :update_user_id, '%')) 
+    AND a."TRANS_ID" = :trans_id 
+    AND UPPER (RTRIM (RTRIM (a."TRANS_INFO"), '　')) LIKE UPPER ('%' || :trans_info || '%') 
+    AND RTRIM (RTRIM (a."STATUS_KB"), '　') IN (:status_kb) 
+    AND a."INSERT_TS" = TO_TIMESTAMP (REPLACE (SUBSTR (:insert_ts, 0, 23), 'T', ' '), 'YYYY-MM-DD HH24:MI:SS.FF3') 
+    AND a."INSERT_TS" >= TO_TIMESTAMP (REPLACE (SUBSTR (:insert_ts_1 , 0, 23), 'T', ' '), 'YYYY-MM-DD HH24:MI:SS.FF3')
+    AND a."INSERT_TS" <= TO_TIMESTAMP (REPLACE (SUBSTR (:insert_ts_2 , 0, 23), 'T', ' '), 'YYYY-MM-DD HH24:MI:SS.FF3')
+    AND UPPER (RTRIM (RTRIM (a."INSERT_USER_ID"), '　')) LIKE UPPER ('%' || :insert_user_id || '%') 
+    AND a."UPDATE_TS" = TO_TIMESTAMP (REPLACE (SUBSTR (:update_ts, 0, 23), 'T', ' '), 'YYYY-MM-DD HH24:MI:SS.FF3') 
+    AND a."UPDATE_TS" >= TO_TIMESTAMP (REPLACE (SUBSTR (:update_ts_1 , 0, 23), 'T', ' '), 'YYYY-MM-DD HH24:MI:SS.FF3')
+    AND a."UPDATE_TS" <= TO_TIMESTAMP (REPLACE (SUBSTR (:update_ts_2 , 0, 23), 'T', ' '), 'YYYY-MM-DD HH24:MI:SS.FF3')
+    AND UPPER (RTRIM (RTRIM (a."UPDATE_USER_ID"), '　')) LIKE UPPER ('%' || :update_user_id || '%') 
 ORDER BY
-    a.`TRANS_ID`
+    a."TRANS_ID"
--- samplecom/src/main/resources/sql/V14FukaSagyokuSearch.sql
+++ samplecom/src/main/resources/sql/V14FukaSagyokuSearch.sql
@@ -1,27 +1,17 @@
 SELECT
-    DISTINCT
-      '''' || LISTAGG (DISTINCT a.labels, ''',''') WITHIN GROUP (ORDER BY a.labels) OVER (PARTITION BY a.type) || '''' AS "labels" 
-    , a.type 
-    , a.stack 
-    , a.label 
-    , LISTAGG (a.DATA, ',') WITHIN GROUP (ORDER BY a.labels) OVER (PARTITION BY a.type, a.label) AS DATA 
-FROM 
-    ( 
-SELECT 
-      a.`labels` AS `labels`
-    , a.`type` AS `type`
-    , TRIM(TRAILING ' ' FROM a.`stack`) AS `stack`
-    , TRIM(TRAILING ' ' FROM a.`label`) AS `label`
-    , a.`DATA` AS `DATA`
+      a."LABELS" AS "LABELS"
+    , RTRIM (RTRIM (a."TYPE"), '　') AS "TYPE"
+    , RTRIM (RTRIM (a."STACK"), '　') AS "STACK"
+    , RTRIM (RTRIM (a."LABEL"), '　') AS "LABEL"
+    , a."DATA" AS "DATA"
 FROM
     V14_FUKA_SAGYOKU a 
 WHERE
     1 = 1 
-    AND UPPER (TRIM(TRAILING ' ' FROM a.`labels`)) LIKE UPPER (CONCAT ('%', :labels, '%')) 
-    AND a.`labels` >= :labels_1 
-    AND a.`labels` <= :labels_2 
-    AND UPPER (TRIM(TRAILING ' ' FROM a.`type`)) LIKE UPPER (CONCAT ('%', :type, '%')) 
-    AND UPPER (TRIM(TRAILING ' ' FROM a.`stack`)) LIKE UPPER (CONCAT ('%', :stack, '%')) 
-    AND UPPER (TRIM(TRAILING ' ' FROM a.`label`)) LIKE UPPER (CONCAT ('%', :label, '%')) 
-    AND UPPER (TRIM(TRAILING ' ' FROM a.`DATA`)) LIKE UPPER (CONCAT ('%', :data, '%')) 
-    ) a 
+    AND UPPER (RTRIM (RTRIM (a."LABELS"), '　')) LIKE UPPER ('%' || :labels || '%') 
+    AND a."LABELS" >= :labels_1 
+    AND a."LABELS" <= :labels_2 
+    AND UPPER (RTRIM (RTRIM (a."TYPE"), '　')) LIKE UPPER ('%' || :type || '%') 
+    AND UPPER (RTRIM (RTRIM (a."STACK"), '　')) LIKE UPPER ('%' || :stack || '%') 
+    AND UPPER (RTRIM (RTRIM (a."LABEL"), '　')) LIKE UPPER ('%' || :label || '%') 
+    AND a."DATA" = :data 
