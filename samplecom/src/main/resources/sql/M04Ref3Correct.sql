7d79e3f749c20fe2d42d94d77ecc06a011340504
ToshiyukiFukuo <t.fukuo.0002@gmail.com>
2026-09-18 20:18:07 +0900

    制約モデル見直し

--- samplecom/src/main/resources/sql/M04Ref3Correct.sql
+++ samplecom/src/main/resources/sql/M04Ref3Correct.sql
@@ -34,5 +34,18 @@ WHERE
     AND a."UPDATE_TS" >= TO_TIMESTAMP (REPLACE (SUBSTR (:update_ts_1 , 0, 23), 'T', ' '), 'YYYY-MM-DD HH24:MI:SS.FF3')
     AND a."UPDATE_TS" <= TO_TIMESTAMP (REPLACE (SUBSTR (:update_ts_2 , 0, 23), 'T', ' '), 'YYYY-MM-DD HH24:MI:SS.FF3')
     AND UPPER (RTRIM (RTRIM (a."UPDATE_USER_ID"), '　')) LIKE UPPER ('%' || :update_user_id || '%') 
+    AND EXISTS ( 
+        SELECT
+              * 
+        FROM
+            T04_COMP2 p 
+        WHERE
+            1 = 1 
+            AND NVL (p.tekiyo_bi, SYSDATE) <= SYSDATE 
+            AND p.REF1_ID = :ref_1_id 
+            AND p.REF2_ID = :ref_2_id 
+            AND p.REF3_ID = :ref_3_id 
+            AND p.TEKIYO_BI = a.TEKIYO_BI 
+    ) 
 ORDER BY
     a."REF3_ID"
--- samplecom/src/main/resources/sql/MhrUserCorrect.sql
+++ samplecom/src/main/resources/sql/MhrUserCorrect.sql
@@ -37,5 +37,19 @@ WHERE
     AND a."UPDATE_TS" >= TO_TIMESTAMP (REPLACE (SUBSTR (:update_ts_1 , 0, 23), 'T', ' '), 'YYYY-MM-DD HH24:MI:SS.FF3')
     AND a."UPDATE_TS" <= TO_TIMESTAMP (REPLACE (SUBSTR (:update_ts_2 , 0, 23), 'T', ' '), 'YYYY-MM-DD HH24:MI:SS.FF3')
     AND UPPER (RTRIM (RTRIM (a."UPDATE_USER_ID"), '　')) LIKE UPPER ('%' || :update_user_id || '%') 
+    AND EXISTS ( 
+        SELECT
+              * 
+        FROM
+            MHR_USER_POS p 
+        WHERE
+            1 = 1 
+            AND NVL (p.tekiyo_bi, SYSDATE) <= SYSDATE 
+            AND NVL (p.haishi_bi, SYSDATE) + 1 > SYSDATE
+            AND p.BUSHO_ID = :busho_id 
+            AND p.SHOKUI_ID = :shokui_id 
+            AND p.USER_ID = :user_id 
+            AND p.TEKIYO_BI = a.TEKIYO_BI 
+    ) 
 ORDER BY
     a."USER_ID"
